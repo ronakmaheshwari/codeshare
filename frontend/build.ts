2731df50c4280f89be90b5a0da5c948a34f0aa45
ronakmaheshwari <ronakmaheshwari077@gmail.com>
2026-07-25 15:07:34 +0530

    fix: Nginx Issue resolved by bun public path was invalid

--- frontend/build.ts
+++ frontend/build.ts
@@ -129,6 +129,7 @@ const result = await Bun.build({
   minify: true,
   target: "browser",
   sourcemap: "linked",
+  publicPath: "/",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
